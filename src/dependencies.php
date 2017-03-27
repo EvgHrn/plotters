48d75bf644b8d8c2ff527267a3271461ae162e0f
Uj <Uj>
2017-03-27 21:35:20 +0400

    Register twig on container

--- src/dependencies.php
+++ src/dependencies.php
@@ -9,6 +9,19 @@ $container['renderer'] = function ($c) {
     return new Slim\Views\PhpRenderer($settings['template_path']);
 };
 
+// Register twig on container
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig('path/to/templates', [
+        'cache' => 'path/to/cache'
+    ]);
+    
+    // Instantiate and add Slim specific extension
+    $basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
+    $view->addExtension(new Slim\Views\TwigExtension($container['router'], $basePath));
+
+    return $view;
+};
+
 // monolog
 $container['logger'] = function ($c) {
     $settings = $c->get('settings')['logger'];
