5f63ebe1646f37252401e00654d6268ed21a3f25
EvgHrn <evg.hrn@gmail.com>
2017-03-29 17:30:36 +0400

    Add saving GET data to database

--- src/db.php
+++ src/db.php
@@ -16,16 +16,15 @@ class Db
         return new \Slim\PDO\Database($dsn, $usr, $pwd);
     }
 
-    static public function saveData($from, $to)
+    static public function saveData($data)
     {
         $pdo = static::getPDO();
-        $result = [
-            [date(DATE_ATOM, mktime(11, 11, 18, 15, 10, 2010)), 3, 2, 3, 4, 5],
-            [date(DATE_ATOM, mktime(11, 11, 18, 15, 10, 2011)), 3, 0, 3, 4, 5],
-            [date(DATE_ATOM, mktime(11, 11, 18, 15, 10, 2012)), 2, 2, 3, 4, 10],
-            [date(DATE_ATOM, mktime(11, 11, 18, 15, 10, 2013)), 3, 8, 3, 4, 5],
-            [date(DATE_ATOM, mktime(11, 11, 18, 15, 10, 2014)), 3, 2, 3, 4, 0]
-        ];
-        return $result;
+        $insertStatement = $pdo->insert(array('session_id', 'plotter', 'start_datetime', 
+                                                'stop_datetime', 'passes', 'meters'))
+                            ->into('printsessions')
+                            ->values(array($data['session_id'], $data['plotter'], $data['start_datetime'], 
+                                                $data['stop_datetime'], $data['passes'], $data['meters']));
+
+        $insertId = $insertStatement->execute(false);
     }
 }
--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'parser.php';
+require_once 'db.php';
 
 $app->get('/', function ($request, $response, $args) {
 
@@ -26,21 +27,13 @@ $app->get('/getdata', function ($request, $response, $args) {
     return $this->view->render($response, 'index.twig', $data);
 });
 
-$app->post('/postdata', function ($request, $response, $args) {
+$app->get('/postdata', function ($request, $response, $args) {
 
+    // var_dump($request->getQueryParams());
+    // die();
     
+    $data = $request->getQueryParams();
 
-    var_dump($request->getQueryParams());
-    die();
-    // // Sample log message
-    // $this->logger->info("Slim-Skeleton '/' route");
+    Db::saveData($data);
 
-    // $params = $request->getQueryParams();
-
-    // $from = $params['dateTimeFrom'];
-    // $to = $params['dateTimeTo'];
-    
-    // $data = Parser::getData($from, $to);
-
-    // return $this->view->render($response, 'index.twig', $data);
 });
