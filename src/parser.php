aac6adcef11ed151d70731d6da325fd0e8967d70
EvgHrn <evg.hrn@gmail.com>
2017-03-29 17:37:18 +0400

    Edit .htaccess

--- src/parser.php
+++ src/parser.php
@@ -19,13 +19,21 @@ class Parser
     static public function getData($from, $to)
     {
         $pdo = static::getPDO();
-        $result = [
-            [date(DATE_ATOM, mktime(11, 11, 18, 15, 10, 2010)), 3, 2, 3, 4, 5],
-            [date(DATE_ATOM, mktime(11, 11, 18, 15, 10, 2011)), 3, 0, 3, 4, 5],
-            [date(DATE_ATOM, mktime(11, 11, 18, 15, 10, 2012)), 2, 2, 3, 4, 10],
-            [date(DATE_ATOM, mktime(11, 11, 18, 15, 10, 2013)), 3, 8, 3, 4, 5],
-            [date(DATE_ATOM, mktime(11, 11, 18, 15, 10, 2014)), 3, 2, 3, 4, 0]
-        ];
+
+        $selectStatement = $pdo->select()
+                           ->from('printsessions')
+                           ->where('id', '=', 1234);
+
+        $stmt = $selectStatement->execute();
+        $data = $stmt->fetch();
+
+        // $result = [
+        //     [date(DATE_ATOM, mktime(11, 11, 18, 15, 10, 2010)), 3, 2, 3, 4, 5],
+        //     [date(DATE_ATOM, mktime(11, 11, 18, 15, 10, 2011)), 3, 0, 3, 4, 5],
+        //     [date(DATE_ATOM, mktime(11, 11, 18, 15, 10, 2012)), 2, 2, 3, 4, 10],
+        //     [date(DATE_ATOM, mktime(11, 11, 18, 15, 10, 2013)), 3, 8, 3, 4, 5],
+        //     [date(DATE_ATOM, mktime(11, 11, 18, 15, 10, 2014)), 3, 2, 3, 4, 0]
+        // ];
         return $result;
     }
 }
